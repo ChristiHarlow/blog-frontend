ed6c97c0f14429d556f7fbe3361c49532760492e
Christi Harlow <cah120671@gmail.com>
2022-08-17 18:04:01 -0400

    Update APIUrl.js

--- src/APIUrl.js
+++ src/APIUrl.js
@@ -1,3 +1,8 @@
-const APIUrl = "http://https://christiharlow-blog-backend.herokuapp.com/";
+let APIUrl = "http://localhost:3001";
+
+// handle if we are running heroku
+if (window.location.host.indexOf(".herokuapp.com") !== -1) {
+    APIUrl = "https://christiharlow-blog-backend.herokuapp.com/";
+}
 
 export default APIUrl;
